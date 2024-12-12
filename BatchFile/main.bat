f410472f774d7a498b336a3ebbe23b8fe035a57c
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-12-12 16:53:29 -0300

    UPDAAATE!!11! 🥳🥳🥳

---
+++ BatchFile/main.bat
@@ -0,0 +1,15 @@
+:: github-commit-max 
+:: PatoFlamejanteTV/UltimateQuack
+
+@echo off
+TITLE Main.BAT
+
+:: ugly
+start generate.bat
+start generate.bat
+start generate.bat
+start generate.bat
+
+:main
+git push
+goto main
---
+++ BatchFile/update.bat
@@ -0,0 +1,5 @@
+@echo off :: Disables output
+TITLE Update.BAT
+git add .
+git commit -m "UPDAAATE!!11! 🥳🥳🥳"
+git push
