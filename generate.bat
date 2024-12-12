7df6c6c0a3b88106502e21dca36326980e440b6c
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-12-12 16:54:11 -0300

    folder

--- generate.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-TITLE Generate.BAT
-
-:main
-git commit --allow-empty -m "1 MORE TO THE LIST 🥳"
-goto main
--- update.bat
+++
@@ -1,5 +0,0 @@
-@echo off :: Disables output
-TITLE Update.BAT
-git add .
-git commit -m "UPDAAATE!!11! 🥳🥳🥳"
-git push
